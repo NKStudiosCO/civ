5cd876d2c9571556277c935cba4d3a30f56967ee
Nikolas Karinja <nikobikoliko@gmail.com>
2016-12-21 10:00:30 -0800

    Update audio56.js

--- audio56.js
+++ audio56.js
@@ -1,8 +1,2 @@
- var audio = document.createElement("audio");
-function play_sound(value){
-  if (options_audio_sound_effects_enabled === 1){
-    audio.src = value;
-    audio.play();
-  }
-}
+
  play_sound("./sound/menu.mp3");
